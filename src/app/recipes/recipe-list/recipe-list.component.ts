b9c9f926f04a54a600714110e3f92cd1b55a0294
vijeth <vijeth11@yahoo.co.in>
2019-09-15 20:59:14 +0530

    added recipes list and recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('A test Recipe','this is a test recipe','http://www.alaskafit.com/wp-content/uploads/2017/02/Recipes-Banner-300x169.jpg'),
+    new Recipe('A test Recipe','this is a test recipe','http://www.alaskafit.com/wp-content/uploads/2017/02/Recipes-Banner-300x169.jpg'),
+
+  ];
 
   constructor() { }
 
