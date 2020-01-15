92ce106f23566573d6df84895487e5d9387e54f6
vijeth <vijeth11@yahoo.co.in>
2020-01-16 10:26:40 +1200

    updated firebase link

--- angular Projects/http-01-start/src/app/app.component.ts
+++ angular Projects/http-01-start/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://angular-comlete-guide-udemy.firebaseio.com/post.json',
         postData
       )
       .subscribe(responseData => {
