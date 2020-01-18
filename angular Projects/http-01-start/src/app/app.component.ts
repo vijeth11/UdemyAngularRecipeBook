c8c8977b2210d38127bf54cf487543ac1f2c02cd
vijeth <vijeth11@yahoo.co.in>
2020-01-19 07:46:17 +1200

    added error handleings and header setting

--- angular Projects/http-01-start/src/app/app.component.ts
+++ angular Projects/http-01-start/src/app/app.component.ts
@@ -1,24 +1,34 @@
 import { PostService } from './post.service';
 import { Post } from './posts.module';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,OnDestroy {
+  
   loadedPosts:Post[] = [];
   isloading:boolean=false;
-
+  error:string=null;
+  private errorSubscription:Subscription;
   constructor(private http: HttpClient,private postService:PostService) {}
 
   ngOnInit() {
+    this.errorSubscription = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
+
     this.isloading = true;
     this.postService.fetchPosts().subscribe(responseData => {
       this.isloading = false;
       this.loadedPosts = responseData;
+    },error => {
+      this.isloading = false;
+      this.error = error.message;
     });
   }
 
@@ -33,11 +43,23 @@ export class AppComponent implements OnInit {
     this.postService.fetchPosts().subscribe(responseData => {
       this.isloading = false;
       this.loadedPosts = responseData;
+    }, error => {
+      this.isloading = false;
+      this.error = error.message;
     });
   }
 
   onClearPosts() {
     // Send Http request
+    this.postService.deleteAllPost().subscribe(()=>{
+      this.loadedPosts = [];
+    });
   }
 
+  onHandleError(){
+    this.error = null;
+  }
+  ngOnDestroy(): void {
+    this.errorSubscription.unsubscribe();
+  }
 }
--- angular Projects/http-01-start/src/app/post.service.ts
+++ angular Projects/http-01-start/src/app/post.service.ts
@@ -1,11 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './posts.module';
-import {map} from 'rxjs/operators'
+import {map, catchError} from 'rxjs/operators'
+import { Subject, throwError } from 'rxjs';
 @Injectable({
     providedIn:'root'
 })
 export class PostService {
+  error:Subject<string> = new Subject<string>();
     constructor(private http:HttpClient){}
     
     createAndStorePost(title:string,content:string){
@@ -17,21 +19,35 @@ export class PostService {
       )
       .subscribe(responseData => {
         console.log(responseData);
+      },error => {
+        this.error.next(error.message);
       });
     }
     fetchPosts(){
+      let searchParams:HttpParams = new HttpParams();
+       searchParams = searchParams.append('printt','pretty');
+       searchParams = searchParams.append('Custom','key');
         return this.http
-        .get<{[id:string]:Post}>('https://angular-comlete-guide-udemy.firebaseio.com/post.json')
-        .pipe(map((responsedata:{[postId:string]:Post}) => {
+        .get<{[id:string]:Post}>('https://angular-comlete-guide-udemy.firebaseio.com/post.json',
+        {
+          headers:new HttpHeaders({'Custom-Header':'hello'}),
+          //params: new HttpParams().set('print','pretty')
+          params:searchParams
+        })
+        .pipe(
+          map((responsedata:{[postId:string]:Post}) => {
           var postsArray=[]
           for(var key in responsedata){
             postsArray.push({...responsedata[key],id:key});
           }
           return postsArray
+        }),
+        catchError(errorRes => {
+          return throwError(errorRes);
         }));
     }
 
-    deleteAllPost(idList:string[]){
-      
+    deleteAllPost(){
+      return this.http.delete('https://angular-comlete-guide-udemy.firebaseio.com/post.json')
     }
 }
